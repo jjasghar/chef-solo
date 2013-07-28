9d6fa755902aeb00ee680dcc2a53940f6723c4eb
JJ Asghar <jjasghar@gmail.com>
2013-07-28 01:07:06 -0500

    added pkgs

--- cookbooks/generic/recipes/packages.rb
+++ cookbooks/generic/recipes/packages.rb
@@ -1,7 +1,7 @@
 
 # packages
 
-%w{build-essential wget libyaml-dev zlib1g-dev libreadline-dev libssl-dev tk-dev libgdbm-dev vim git-core}.each do |pkg|
+%w{build-essential wget libyaml-dev zlib1g-dev libreadline-dev libssl-dev tk-dev libgdbm-dev vim git-core zsh}.each do |pkg|
   package pkg do
     action [:install]
   end
