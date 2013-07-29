ae3204fd0c3903160660740f861bcf40085f204d
JJ Asghar <jjasghar@gmail.com>
2013-07-29 10:34:30 -0500

    i'm able to get the root .zshrc in

--- cookbooks/generic/recipes/default.rb
+++ cookbooks/generic/recipes/default.rb
@@ -1,6 +1,7 @@
 
 include_recipe "generic::ntp"
 include_recipe "generic::packages"
+include_recipe "generic::shells"
 
 
 
--- cookbooks/generic/recipes/shells.rb
+++ cookbooks/generic/recipes/shells.rb
@@ -1,6 +1,6 @@
 
 
-cookbook_file 'zshrc" do
-  path "/root/.zshrc
+cookbook_file "zshrc" do
+  path "/root/.zshrc"
   action :create_if_missing
 end
